dafa40f2ab91add8196807b08f39a4ece9937355
bayuajik2403 <bayuajik2403@gmail.com>
2020-10-24 19:28:54 +0700

    get all data

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 const User = require('./models/User')
 const express = require('express');
+const bodyParser =require('body-parser');
+
 const app = express();
 const port = 8081 || process.env.PORT;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.Promise = global.Promise;
 
 
 mongoose.connect('mongodb://localhost:27017/mongoose');
@@ -12,18 +18,37 @@ mongoose.connection
         console.log(`couldnt connect `, err)
     });
 
-app.post('/users');
 
-// const newUser = new User({
-//     firstName : 'Bayu Ajikkkk',
-//     lastName : 'Kurniawan',
-//     isActive : 1
-// });
+app.get('/', (req,res)=>{
+    res.send('ROOT');
+});
 
-// newUser.save(function(err,dataSaved){
-//     if(err) return err;
-//     console.log(`data is saved: `+dataSaved);
-// });
+app.post('/users',(req, res)=>{
+    const newUser = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        isActive : req.body.isActive
+    });
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+    // newUser.save(function(err,dataSaved){
+    //     if(err) return err;
+    //     console.log(`data is saved: `+dataSaved);
+    // });
+
+    newUser.save().then(savedUser=>{
+        console.log('saved user')
+        res.send('USER SAVED')
+    }).catch(err=>{
+        res.status(404).send('USER NOT SAVE BECAUSE.....');
+    });
+
+});
+
+//get request all data
+app.get('/users',(req,res)=>{
+    User.find({}).then(users=>{
+        res.status(200).send(users);
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!!!`))
