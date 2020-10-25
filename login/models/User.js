f0906771a96e3dd88f5363d0e3beb95050a376de
bayuajik2403 <bayuajik2403@gmail.com>
2020-10-25 23:22:31 +0700

    add user model

--- login/models/User.js
+++ login/models/User.js
@@ -2,5 +2,18 @@ const mongoose =require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    
+   email: {
+       type: String,
+       unique: true,
+       trim: true,
+       minlength: 3,
+       required: true
+   }, 
+   password: {
+       type: String,
+       minlength: 8,
+       required: true
+   }
 });
+
+module.exports = mongoose.model('user', UserSchema)
